33067426207241f1689cae5e175db835ef8bc22c
admin <admin@YD2-21>
2022-05-17 09:29:30 +0900

    20220517

---
+++ P20220415/src/Co/edu/Ex1.java
@@ -0,0 +1,18 @@
+package Co.edu;
+
+import java.util.Scanner;
+
+public class Ex1 {
+	public static void main(String[] args) {
+		
+		//1.계좌생성 2.입금 3.출금 4.목록조회 5.종료
+		//Account : 계좌번호, 예금주, 잔액
+		//AccountApp : 1.계좌생성 2.입금 3.출금 4.목록조회 5.종료
+		//AccountExe : AccountApp 클래스의 인스턴스를 호출해서 execute();
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+	}
+
+}
